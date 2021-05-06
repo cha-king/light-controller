0ca81e308c2b1c0451030190327a8893f5635e11
Charlie King <charlie.king@verkada.com>
2021-05-05 20:26:51 -0700

    Base commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/cha-king/light-controller
+
+go 1.15
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.3.4
+	github.com/stianeikeland/go-rpio v4.2.0+incompatible
+)
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	rpio "github.com/stianeikeland/go-rpio"
+)
+
+const (
+	lampPin   = 24
+	brokerUri = "tcp://broker.home.cha-king.com:1883"
+	topicBase = "bedroom/lamp"
+	topic     = topicBase + "/setState"
+	qos       = 1
+)
+
+var pin rpio.Pin
+
+func handleMessage(client mqtt.Client, message mqtt.Message) {
+	val := string(message.Payload())
+	log.Printf("Message received: %s", val)
+	switch val {
+	case "on":
+		pin.High()
+	case "off":
+		pin.Low()
+	}
+}
+
+func main() {
+	rpio.Open()
+	defer rpio.Close()
+	pin = rpio.Pin(lampPin)
+
+	clientOptions := mqtt.NewClientOptions()
+	clientOptions.AddBroker(brokerUri)
+	client := mqtt.NewClient(clientOptions)
+
+	token := client.Connect()
+	defer client.Disconnect(0)
+	token.Wait()
+	if token.Error() != nil {
+		log.Fatal(token.Error())
+	}
+	log.Printf("Connected to %s", brokerUri)
+
+	token = client.Subscribe(topic, qos, handleMessage)
+	token.Wait()
+
+	quit := make(chan os.Signal, 8)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Print("Exiting..")
+}
